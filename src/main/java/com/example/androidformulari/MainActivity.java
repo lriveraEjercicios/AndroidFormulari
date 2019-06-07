00bfc665900981cb7a8d1831f424b5dfd72efbac
Jordi Boix <jboixbaro@gmail.com>
2019-06-07 13:55:21 +0200

    Added AlertDialog on Cancel

--- src/main/java/com/example/androidformulari/MainActivity.java
+++ src/main/java/com/example/androidformulari/MainActivity.java
@@ -37,8 +37,39 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Todo OK", Toast.LENGTH_LONG).show();
         }
     }
+    private void deleteContent()
+    {
+        etName.setText("");
+        etMail.setText("");
+        etPhone.setText("");
+        etPassword.setText("");
+    }
+    public void btnCancelPressed(View view)
+    {
+        openDialog();
+    }
 
-   
+    public void openDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+        alertDialogBuilder.setTitle(getString(R.string.app_name)); //modificar el titol
+        alertDialogBuilder.setMessage( getString(R.string.questionDelete))
+                .setCancelable(false)
+                .setPositiveButton(getString(R.string.btn_yes), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        deleteContent();
+                    }
+                })
+                .setNegativeButton(getString(R.string.btn_no), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel(); // No fem res, tanquem el Alert.
+
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create(); //crear el alert dialog
+        alertDialog.show(); //mostrar per pantalla
+    }
 
 
 }
