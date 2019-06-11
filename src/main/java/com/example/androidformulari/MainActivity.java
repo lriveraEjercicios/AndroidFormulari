c533eaca8a09e7c6effe3f1a3dc547c2709850ac
Jordi Boix <jboixbaro@gmail.com>
2019-06-11 10:26:08 +0200

    added datepicker with minDate

--- src/main/java/com/example/androidformulari/MainActivity.java
+++ src/main/java/com/example/androidformulari/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.androidformulari;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etName, etPhone, etMail, etPassword;
+    EditText etName, etPhone, etMail, etPassword,etDate;
+    int pDay, pMonth, pYear = 0;
+    Activity activity = this;
+
+    public DatePickerDialog.OnDateSetListener pDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    pYear = year;
+                    pMonth = month;
+                    pDay = dayOfMonth;
+
+                    String fromDay = Integer.toString(pDay);
+                    String fromMonth = Integer.toString(pMonth + 1);
+
+                    if (fromDay.length() == 1) fromDay = "0" + fromDay;
+                    if (fromMonth.length() == 1) fromMonth = "0" + fromMonth;
+
+                    etDate.setText(fromDay + "/" + fromMonth + "/" + pYear);
+                }
+            };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +44,22 @@ public class MainActivity extends AppCompatActivity {
         etPhone = findViewById(R.id.etName);
         etMail = findViewById(R.id.etName);
         etPassword = findViewById(R.id.etName);
+        etDate = findViewById(R.id.etDate);
+
+        etDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Obrir el datepicker
+                DatePickerDialog dateDialog =
+                        new DatePickerDialog(activity,
+                                pDateSetListener,
+                                pYear, pMonth, pDay);
+                //Minima data seleccionable, avui:
+                dateDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                dateDialog.show();
+
+            }
+        });
 
     }
 
